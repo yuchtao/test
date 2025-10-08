d17ac6869732ff167a2232321fdd1e22ebabcdac
keyuqi <#>
2025-10-08 17:36:20 +0800

    test2修改的方法第一次修改

--- Hello.java
+++ Hello.java
@@ -3,5 +3,7 @@ public class Hello {
         System.out.println("Hello World!");
         System.out.println("Hello World2");
         System.out.println("Hello World3");
+        // test2修改的方法
+        // test2修改的方法第一次修改
     }
 }
