8f83a207a5d0e147f191fcbce2bb48a35368dfb2
keyuqi <#>
2025-10-08 20:55:38 +0800

    Revert "新增HelloTest方法"
    This reverts commit 7a42444c04c070b054ed28d9eecde78bb6bec119.

--- HelloTest.java
+++
@@ -1,9 +0,0 @@
-public class HelloTest {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World2");
-        System.out.println("Hello World3");
-        // test1修改的方法
-        // test1修改的方法第二次修改
-    }
-}
